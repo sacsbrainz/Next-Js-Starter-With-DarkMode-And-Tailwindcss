9891641b2d31a03689549c44f26271e31e86ca3e
sacsbrainz <solomonogu0@gmail.com>
2022-03-24 20:26:48 +0100

    first push -added DarkMode to the  next-js starter

---
+++ components/Header.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+// import Logo from 
+import {useTheme} from 'next-themes'
+import { MoonIcon, SunIcon } from '@heroicons/react/solid'
+
+function Header() {
+    const [mounted, setMounted] = useState(false)
+    
+    useEffect(() => {
+      setMounted(true)
+    }, [])
+    
+    const {systemTheme, theme, setTheme} = useTheme()
+    const rendeThemeChanger = () => {
+        if(!mounted) return null
+        const currentTheme = theme === 'system' ? systemTheme : theme
+
+        if (currentTheme === 'dark') {
+            return (
+                <SunIcon className='w-7 h-7' role='button' onClick={() => setTheme ('light')} />
+            )
+        }
+        else {
+            return (<MoonIcon className='w-7 h-7' role='button' onClick={() => setTheme ('dark')}/>)}
+    }
+  return (
+    <div>
+        <header className='border-b border-gray-100 dark:border-gray-700'>
+            <div className='container mx-auto px-4 sm:px-6 py-4 flex justify-center items-center'>
+                {rendeThemeChanger()}
+            </div>
+        </header>
+    </div>
+  )
+}
+
+export default Header
